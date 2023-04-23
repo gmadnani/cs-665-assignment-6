471a674bc60c3a87e5145115985168a86d09b3cf
Girish Madnani <girishmadnani05@gmail.com>
2023-04-23 18:11:26 -0400

    updating enum in email factory

--- src/main/java/edu/bu/met/cs665/emails/EmailFactory.java
+++ src/main/java/edu/bu/met/cs665/emails/EmailFactory.java
@@ -8,20 +8,8 @@
 
 package edu.bu.met.cs665.emails;
 
-import edu.bu.met.cs665.customers.BusinessCustomer;
-import edu.bu.met.cs665.customers.FrequentCustomer;
-import edu.bu.met.cs665.customers.NewCustomer;
-import edu.bu.met.cs665.customers.ReturningCustomer;
-import edu.bu.met.cs665.customers.VipCustomer;
-
+import edu.bu.met.cs665.customers.Customer;
+import edu.bu.met.cs665.customers.CustomerType;
 public interface EmailFactory {
-  BusinessCustomer createBusinessCustomer(String name);
-  
-  ReturningCustomer createReturningCustomer(String name);
-  
-  FrequentCustomer createFrequentCustomer(String name);
-  
-  NewCustomer createNewCustomer(String name);
-  
-  VipCustomer createVipCustomer(String name);
+  Customer createCustomer(String name, CustomerType type);
 }
--- src/main/java/edu/bu/met/cs665/emails/EmailGenerationFactory.java
+++ src/main/java/edu/bu/met/cs665/emails/EmailGenerationFactory.java
@@ -10,29 +10,28 @@
 package edu.bu.met.cs665.emails;
 
 import edu.bu.met.cs665.customers.BusinessCustomer;
+import edu.bu.met.cs665.customers.Customer;
+import edu.bu.met.cs665.customers.CustomerType;
 import edu.bu.met.cs665.customers.FrequentCustomer;
 import edu.bu.met.cs665.customers.NewCustomer;
 import edu.bu.met.cs665.customers.ReturningCustomer;
 import edu.bu.met.cs665.customers.VipCustomer;
 
 public class EmailGenerationFactory implements EmailFactory {
-  public BusinessCustomer createBusinessCustomer(String name) {
-    return new BusinessCustomer(name);
-  }
-  
-  public ReturningCustomer createReturningCustomer(String name) {
-    return new ReturningCustomer(name);
-  }
-  
-  public FrequentCustomer createFrequentCustomer(String name) {
-    return new FrequentCustomer(name);
-  }
-  
-  public NewCustomer createNewCustomer(String name) {
-    return new NewCustomer(name);
-  }
-  
-  public VipCustomer createVipCustomer(String name) {
-    return new VipCustomer(name);
+  public Customer createCustomer(String name, CustomerType type) {
+    switch (type) {
+      case BUSINESS:
+        return new BusinessCustomer(name);
+      case RETURNING:
+        return new ReturningCustomer(name);
+      case FREQUENT:
+        return new FrequentCustomer(name);
+      case NEW:
+        return new NewCustomer(name);
+      case VIP:
+        return new VipCustomer(name);
+      default:
+        throw new IllegalArgumentException("Invalid customer type");
+    }
   }
 }
